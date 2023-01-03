7ba7da5ef2612d87041ca63754f091188816ac77
Patrick-AA <patrick.abiabdallah@outlook.com>
2023-01-03 16:54:51 +0100

    added scout mini

--- scout_description/launch/scout_base_description.launch.py
+++ scout_description/launch/scout_base_description.launch.py
@@ -1,34 +1,33 @@
-import os
 import launch
 import launch_ros
-
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import FindExecutable, PathJoinSubstitution
-from launch.substitutions import LaunchConfiguration, Command
-from launch_ros.actions import Node
+from launch.substitutions import Command
 
 
 def generate_launch_description():
-    model_name = 'scout_v2.xacro'
-    # model_path = os.path.join(get_package_share_directory('scout_description'), "urdf", model_name)
-    # print(model_path)
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='false',
+                                             description='Use simulation clock if true')
+
+    model_name_arg = DeclareLaunchArgument('model_name', default_value='scout_mini.xacro',
+                                           description='The xarco filename of the robot model.')
+
     robot_description_content = Command([
         PathJoinSubstitution([FindExecutable(name="xacro")]), " ",
         PathJoinSubstitution(
-            [FindPackageShare("scout_description"), "urdf", model_name]
+            [FindPackageShare("scout_description"), "urdf",
+             launch.substitutions.LaunchConfiguration('model_name')]
         ),
     ])
 
     return launch.LaunchDescription([
-        DeclareLaunchArgument('use_sim_time', default_value='false',
-            description='Use simulation clock if true'),
-
+        use_sim_time_arg,
+        model_name_arg,
         launch.actions.LogInfo(msg='use_sim_time: '),
-        launch.actions.LogInfo(msg=launch.substitutions.LaunchConfiguration('use_sim_time')),
-        
+        launch.actions.LogInfo(
+            msg=launch.substitutions.LaunchConfiguration('use_sim_time')),
+
         launch_ros.actions.Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -36,6 +35,11 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time'),
-                'robot_description':robot_description_content
+                'robot_description': robot_description_content
             }]),
+
+        launch_ros.actions.Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher')
     ])
